c19643287b8ba1fc20349dd83ee1eb1dea64d79d
Criston <criston2011@gmail.com>
2019-09-21 23:26:43 +0530

    👌 IMPROVE: index

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,8 @@ import React from "react";
 import Link from "next/link";
 
 const links = [
-  { href: "https://zeit.co/now", label: "Criston Mascarenhas" },
-  { href: "https://github.com/zeit/next.js", label: "GitHub" }
+  { href: "https://github.com/crstnmac/succotash", label: "Succotash" },
+  { href: "https://github.com/crstnmac/succotash", label: "GitHub" }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`;
   return link;
